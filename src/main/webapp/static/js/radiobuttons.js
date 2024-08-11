a2b3678484b62c25ee875d669188629f3d1cd96e
Jaime <jaimecobo@outlook.com>
2024-08-11 08:32:35 -0500

    081024 commit radiobuttons.js

--- src/main/webapp/static/js/radiobuttons.js
+++ src/main/webapp/static/js/radiobuttons.js
@@ -1,22 +1,16 @@
-// Function to handle radio button selection
-function checkRadioButton() {
-    // Get references to the radio buttons
-    var unfollow = document.getElementById('unfollow');
-    var follow = document.getElementById('follow');
 
-    // Determine which radio button is selected
-    if (unfollow.checked) {
+document.getElementById('unfollow').addEventListener('click', function(){
+    if (this.checked) {
         fetch(`/relationship/followedOrNot?status=`+0+`&userId=${userKey.id}`)
-            .then(data => console.log('success'))
-            .catch(error => console.error('Error:', error));
-    } else if (follow.checked) {
-        fetch(`/relationship/followedOrNot?status=`+1+`&userId=${userKey.id}`)
-            .then(data => console.log('success'))
+            .then(data => console.log('success unfollowing'))
             .catch(error => console.error('Error:', error));
     }
+});
 
-    // Event listeners to both radio buttons
-    document.getElementById('unfollow').addEventListener('click', checkRadioButton);
-    document.getElementById('follow').addEventListener('click', checkRadioButton);
-
-}
+document.getElementById('follow').addEventListener('click', function(){
+    if (this.checked) {
+        fetch(`/relationship/followedOrNot?status=`+1+`&userId=${userKey.id}`)
+            .then(data => console.log('success following'))
+            .catch(error => console.error('Error:', error));
+    }
+});
