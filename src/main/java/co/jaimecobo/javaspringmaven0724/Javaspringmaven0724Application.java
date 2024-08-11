caf30267e91aa465dc685c37531b17ee1ef32662
Jaime <jaimecobo@outlook.com>
2024-08-11 08:53:14 -0500

    081124 commit Javaspringmaven0724Application.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/Javaspringmaven0724Application.java
@@ -0,0 +1,13 @@
+package co.jaimecobo.javaspringmaven0724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaspringmaven0724Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaspringmaven0724Application.class, args);
+	}
+
+}
