027e4286886c4e4f8ffdf683917e2c10ad781129
Jaime <jaimecobo@outlook.com>
2024-08-11 08:53:46 -0500

    081124 commit CityImageFileValidator.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/validation/CityImageFileValidator.java
@@ -0,0 +1,22 @@
+package co.jaimecobo.javaspringmaven0724.validation;
+import java.util.Arrays;
+
+public class CityImageFileValidator {
+
+    public boolean isValidFileSize(Long fileSize) {
+        return fileSize <= 6 * 1024 * 1024; // 6 MB limit
+    }
+
+    private String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    public boolean isValidFileType(String fileName, String mimeType) {
+        String[] allowedExtensions = {"jpg", "jpeg", "png"};
+        String[] allowedMimeTypes = {"image/jpeg", "image/png"};
+
+        String fileExtension = getFileExtension(fileName);
+        return Arrays.asList(allowedExtensions).contains(fileExtension) && Arrays.asList(allowedMimeTypes).contains(mimeType);
+    }
+
+}
