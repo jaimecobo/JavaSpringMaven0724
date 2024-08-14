f0ef8d5b5a8b896a6879abb5625fd402c202ef52
Jaime <jaimecobo@outlook.com>
2024-08-13 21:01:23 -0500

    081324 commit CommentDAO.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/CommentDAO.java
@@ -0,0 +1,13 @@
+package co.jaimecobo.javaspringmaven0724.database.dao;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.Comment;
+import co.jaimecobo.javaspringmaven0724.database.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentDAO extends JpaRepository<Comment, Long> {
+    Comment findById(Integer id);
+    List<Comment> findByUserId(Integer id);
+
+}
