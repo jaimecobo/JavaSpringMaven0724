60436f713cee7f45d80e4ded8034514399bab22a
Jaime <jaimecobo@outlook.com>
2024-08-13 21:04:49 -0500

    081324 commit Employee.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/entity/Employee.java
@@ -0,0 +1,34 @@
+package co.jaimecobo.javaspringmaven0724.database.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "create_date", updatable = false, nullable = false, columnDefinition = "timestamp default current_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+}
