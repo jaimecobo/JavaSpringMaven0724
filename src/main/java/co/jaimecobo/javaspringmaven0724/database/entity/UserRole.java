0201cfa098a9b0dfdc43ad0d28c6860f8f84cd08
Jaime <jaimecobo@outlook.com>
2024-08-13 21:06:29 -0500

    081324 commit UserRole.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/entity/UserRole.java
@@ -0,0 +1,31 @@
+package co.jaimecobo.javaspringmaven0724.database.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+
+    @Column(name = "create_date", updatable = false, nullable = false, columnDefinition = "timestamp default current_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+}
