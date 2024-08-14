e13d7fbf125807d325992d28ca38b267a8594391
Jaime <jaimecobo@outlook.com>
2024-08-13 21:02:45 -0500

    081324 commit PostDAO.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/PostDAO.java
@@ -0,0 +1,12 @@
+package co.jaimecobo.javaspringmaven0724.database.dao;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostDAO extends JpaRepository<Post, Long> {
+    Post findById(Integer id);
+
+    List<Post> findByUserId(Integer id);
+}
