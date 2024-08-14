61e0699fb3aa416205a9638e2cb5df8bdaa20aae
Jaime <jaimecobo@outlook.com>
2024-08-13 21:02:17 -0500

    081324 commit EventDAO.java

---
+++ src/main/java/co/jaimecobo/javaspringmaven0724/database/dao/EventDAO.java
@@ -0,0 +1,21 @@
+package co.jaimecobo.javaspringmaven0724.database.dao;
+
+import co.jaimecobo.javaspringmaven0724.database.entity.City;
+import co.jaimecobo.javaspringmaven0724.database.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EventDAO extends JpaRepository<Event, Long> {
+    Event findById(Integer id);
+    List<Event> findByOrganizerId(Integer id);
+    List<Event> findByNameIgnoreCase(String eventName);
+
+    Event findByNameAndCityIdAndStartingDateIgnoreCase(String name, Integer cityId, String startingDate);
+    Event findByNameAndStartingDateIgnoreCase(String name, String startingDate);
+
+    List<Event> findByCity(City city);
+
+}
