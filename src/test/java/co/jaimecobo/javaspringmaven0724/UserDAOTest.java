86fc96b9d38dd4da94dfbbce87d43d76178ec557
Jaime <jaimecobo@outlook.com>
2024-08-15 17:58:39 -0500

    081524 commit UserDAOTest.java

--- src/test/java/co/jaimecobo/javaspringmaven0724/UserDAOTest.java
+++ src/test/java/co/jaimecobo/javaspringmaven0724/UserDAOTest.java
@@ -4,200 +4,19 @@ import co.jaimecobo.javaspringmaven0724.database.dao.UserDAO;
 import co.jaimecobo.javaspringmaven0724.database.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
+@SpringBootTest
 public class UserDAOTest {
 
-    UserDAO userDAO = new UserDAO() {
-        @Override
-        public User findById(Integer id) {
-            return null;
-        }
-
-        @Override
-        public User findByUsernameIgnoreCase(String username) {
-            return null;
-        }
-
-        @Override
-        public User findByEmailIgnoreCase(String email) {
-            return null;
-        }
-
-        @Override
-        public User findByBrandNicknameCompanyIgnoreCase(String brandNicknameCompany) {
-            return null;
-        }
-
-        @Override
-        public List<User> findByFirstNameIgnoreCase(String firstName) {
-            return List.of();
-        }
-
-        @Override
-        public List<User> findByCityIgnoreCase(String city) {
-            return List.of();
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public <S extends User> S saveAndFlush(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends User> List<S> saveAllAndFlush(Iterable<S> entities) {
-            return List.of();
-        }
-
-        @Override
-        public void deleteAllInBatch(Iterable<User> entities) {
-
-        }
-
-        @Override
-        public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAllInBatch() {
-
-        }
-
-        @Override
-        public User getOne(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public User getById(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public User getReferenceById(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public <S extends User> List<S> findAll(Example<S> example) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends User> List<S> saveAll(Iterable<S> entities) {
-            return List.of();
-        }
-
-        @Override
-        public List<User> findAll() {
-            return List.of();
-        }
-
-        @Override
-        public List<User> findAllById(Iterable<Long> longs) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends User> S save(S entity) {
-            return null;
-        }
-
-        @Override
-        public Optional<User> findById(Long aLong) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existsById(Long aLong) {
-            return false;
-        }
-
-        @Override
-        public long count() {
-            return 0;
-        }
-
-        @Override
-        public void deleteById(Long aLong) {
-
-        }
-
-        @Override
-        public void delete(User entity) {
-
-        }
-
-        @Override
-        public void deleteAllById(Iterable<? extends Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<? extends User> entities) {
-
-        }
-
-        @Override
-        public void deleteAll() {
-
-        }
-
-        @Override
-        public List<User> findAll(Sort sort) {
-            return List.of();
-        }
-
-        @Override
-        public Page<User> findAll(Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends User> Optional<S> findOne(Example<S> example) {
-            return Optional.empty();
-        }
-
-        @Override
-        public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends User> long count(Example<S> example) {
-            return 0;
-        }
-
-        @Override
-        public <S extends User> boolean exists(Example<S> example) {
-            return false;
-        }
-
-        @Override
-        public <S extends User, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-            return null;
-        }
-    };
+    @Autowired
+    private UserDAO userDAO;
 
     @Test
     public void findUserByFirstNameIgnoreCase(){
@@ -229,8 +48,21 @@ public class UserDAOTest {
         // When
         List<User> users = userDAO.findAll();
         // Then
-        Assertions.assertTrue(users.size() == 4);
+//        Assertions.assertTrue(users.size() == 4);
+        Assertions.assertEquals(4, users.size());
+
     }
+
+
+
+    @ParameterizedTest
+    @CsvSource({"1, Austin", "2, Los Angeles"})
+    public void findUserById(Integer id, String city){
+        User user = userDAO.findById(id);
+        assert user.getCity().equals(city);
+
+    }
+
 
 
     @Test
@@ -242,7 +74,7 @@ public class UserDAOTest {
         given.setUsername("Test first username");
         given.setEmail("user@test.abc");
         given.setPassword("password12345");
-        given.setGender("This is s test, gender undefined");
+        given.setGender("testing gender");
         given.setPhone("7418529630");
         given.setAddress("741 Seventh St.");
         given.setCity("New York");
@@ -264,9 +96,9 @@ public class UserDAOTest {
         Assertions.assertEquals(given.getCity(), actual.getCity());
         Assertions.assertEquals(given.getState(), actual.getState());
         Assertions.assertEquals(given.getCountry(), actual.getCountry());
-        Assertions.assertEquals(given.getCreatedAt(), actual.getCreatedAt());
         userDAO.delete(given);
         Assertions.assertNull(userDAO.findById(actual.getId()));
+
     }
 
 }
