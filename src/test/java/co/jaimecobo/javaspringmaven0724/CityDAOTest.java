69950c82e0a5e26b1dfb47dd7d0729535198f16e
Jaime <jaimecobo@outlook.com>
2024-08-15 17:58:15 -0500

    081524 commit CityDAOTest.java

--- src/test/java/co/jaimecobo/javaspringmaven0724/CityDAOTest.java
+++ src/test/java/co/jaimecobo/javaspringmaven0724/CityDAOTest.java
@@ -4,190 +4,19 @@ import co.jaimecobo.javaspringmaven0724.database.dao.CityDAO;
 import co.jaimecobo.javaspringmaven0724.database.entity.City;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
+@SpringBootTest
 public class CityDAOTest {
 
-    CityDAO cityDAO = new CityDAO() {
-        @Override
-        public City findById(Integer id) {
-            return null;
-        }
-
-        @Override
-        public List<City> findByNameIgnoreCase(String name) {
-            return List.of();
-        }
-
-        @Override
-        public City findByNameAndStateProvinceDepartmentTerritoryAndCountryIgnoreCase(String name, String stateProvinceDepartmentTerritory, String country) {
-            return null;
-        }
-
-        @Override
-        public List<City> findAllOrderedByCity() {
-            return List.of();
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public <S extends City> S saveAndFlush(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends City> List<S> saveAllAndFlush(Iterable<S> entities) {
-            return List.of();
-        }
-
-        @Override
-        public void deleteAllInBatch(Iterable<City> entities) {
-
-        }
-
-        @Override
-        public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAllInBatch() {
-
-        }
-
-        @Override
-        public City getOne(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public City getById(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public City getReferenceById(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public <S extends City> List<S> findAll(Example<S> example) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends City> List<S> findAll(Example<S> example, Sort sort) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends City> List<S> saveAll(Iterable<S> entities) {
-            return List.of();
-        }
-
-        @Override
-        public List<City> findAll() {
-            return List.of();
-        }
-
-        @Override
-        public List<City> findAllById(Iterable<Long> longs) {
-            return List.of();
-        }
-
-        @Override
-        public <S extends City> S save(S entity) {
-            return null;
-        }
-
-        @Override
-        public Optional<City> findById(Long aLong) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existsById(Long aLong) {
-            return false;
-        }
-
-        @Override
-        public long count() {
-            return 0;
-        }
-
-        @Override
-        public void deleteById(Long aLong) {
-
-        }
-
-        @Override
-        public void delete(City entity) {
-
-        }
-
-        @Override
-        public void deleteAllById(Iterable<? extends Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<? extends City> entities) {
-
-        }
-
-        @Override
-        public void deleteAll() {
-
-        }
-
-        @Override
-        public List<City> findAll(Sort sort) {
-            return List.of();
-        }
-
-        @Override
-        public Page<City> findAll(Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends City> Optional<S> findOne(Example<S> example) {
-            return Optional.empty();
-        }
-
-        @Override
-        public <S extends City> Page<S> findAll(Example<S> example, Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends City> long count(Example<S> example) {
-            return 0;
-        }
-
-        @Override
-        public <S extends City> boolean exists(Example<S> example) {
-            return false;
-        }
-
-        @Override
-        public <S extends City, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-            return null;
-        }
-    };
+    @Autowired
+    private CityDAO cityDAO;
 
     @Test
     public void findCityByNameAndStateProvinceDepartmentTerritoryAndCountryIgnoreCase(){
@@ -202,8 +31,6 @@ public class CityDAOTest {
         Assertions.assertEquals(name, city.getName());
         Assertions.assertEquals(state, city.getStateProvinceDepartmentTerritory());
         Assertions.assertEquals(country, city.getCountry());
-
-
 
     }
 
@@ -227,7 +54,18 @@ public class CityDAOTest {
         // When
         List<City> cities = cityDAO.findAll();
         // Then
-        Assertions.assertTrue(cities.size() == 6);
+//        Assertions.assertTrue(cities.size() == 6);
+        Assertions.assertEquals(6, cities.size());
+
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"1, Austin", "2, Denver", "3, Boulder", "4, Houston"})
+    public void findCityById(Integer id, String cityName){
+        City city = cityDAO.findById(id);
+        assert city.getName().equals(cityName);
+
     }
 
 
@@ -257,10 +95,10 @@ public class CityDAOTest {
         Assertions.assertEquals(given.getSlogan(), actual.getSlogan());
         Assertions.assertEquals(given.getDescription(), actual.getDescription());
         Assertions.assertEquals(given.getLastEditorUser(), actual.getLastEditorUser());
-        Assertions.assertEquals(given.getCreatedAt(), actual.getCreatedAt());
 
         cityDAO.delete(given);
         Assertions.assertNull(cityDAO.findById(actual.getId()));
+
     }
 
 }
